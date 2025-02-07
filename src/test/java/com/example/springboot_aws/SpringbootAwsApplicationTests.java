04b69727592a108e92267495d39984edf315d4b1
tjddn <tjddn3927@naver.com>
2025-02-07 16:27:42 +0900

    feat: project build

---
+++ src/test/java/com/example/springboot_aws/SpringbootAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
